da9d3a22b19c170cacbcc2cdace7b80d33e40443
Dwayne Linde <lindesara@gmail.com>
2016-04-27 20:04:15 -0700

    Updated templates for Dwayneland template
    Updated files for my Dwayneland template.

--- index.php
+++ index.php
@@ -5,13 +5,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>index.html</title>
+<title>about.html</title>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width" />
 <link href='https://fonts.googleapis.com/css?family=Delius+Swash+Caps' rel='stylesheet' type='text/css'>
 
-<link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+    
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="all" />
 
+
+
+    
 </head>
     
 <body>
@@ -30,8 +35,8 @@
     <img src="icons/email.png" class="email" alt="Email link icon." />
     </a>
         
-    
-  <!-- <h2 class="pageid">Home Page</h2> -->
+     
+     <!-- <h2 class="pageid">Home Page</h2> -->
     
   <h3><a href="index.html">Cornerstone Consulting</a></h3>
   <!-- START NAVIGATION (Main) -->
@@ -55,21 +60,20 @@
 </nav>
     
      <!-- <h2 class="pageid">Home Page</h2> -->
-
-    <img src="images/kittens.jpg">
+ 
+    <img src="images/aimee_headshot_sm.gif">
     
-    <h1>HTML Ipsum Presents</h1>
+    <h1>About Cornerstone Consulting</h1>
 	       
-<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
+<p>Aimee Elber, Manager of Disability Support Services at Spokane Community College, holds over a decade of experience in higher education and has worked with people with disabilities since 1994. As a first generation college graduate, Aimee started her academic career in the community college system and completed her Bachelor of Arts degree in Human Services at Western Washington University. She is driven by a deep desire to help students meet their academic and occupational goals.</p>
+    
+    <p>While pursuing her college education, Aimee began her career with the Washington State Division of Vocational Rehabilitation (DVR) as a Governor’s Intern and was later hired as a Rehabilitation Technician. Working with DVR inspired her to seek out opportunities to help people with disabilities successfully enter the workforce. Building on her experience, she worked as a Career Counselor and Academic Advisor at Lake Washington Technical College and later sought employment as a Disability Services Specialist in the Seattle Community Colleges District. She then transitioned to a job in Workforce Education to supplement her existing skills. Aimee served as Assistant Director of Student Services at Pacific Northwest College of Art in Portland, Oregon and later as Coordinator of Disability Services at Clackamas Community College. </p>
+      
+    <p>Aimee adheres to a strengths-based “person first” philosophy, viewing individuals holistically and focusing on assets rather than barriers. She is committed to diversity, multiculturalism, and fostering a safe learning environment. Working with people in a variety of roles has allowed Aimee the opportunity to help individuals become empowered advocates for themselves and their education.</p>
+      
+    <p>Aimee brings a diverse skill set to the table including a broad knowledge base surrounding ADA compliance, Universal Design, Career Counseling theory, Appreciative Inquiry, Veteran’s Services, and Workforce Education. She is proficient in American Sign Language, trained as a Dependable Strengths Level I Facilitator, and certified to administer and interpret the Myers-Briggs Type Indicator.  She is currently pursuing her Master’s degree with a focus in Organizational Leadership.</p>  
 
-<h2>Header Level 2</h2>
-	       
-<ol>
-   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-   <li>Aliquam tincidunt mauris eu risus.</li>
-</ol>
 
-<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>
 
 <h3>Header Level 3</h3>
 
